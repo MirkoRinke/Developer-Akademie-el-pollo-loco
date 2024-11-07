a8470e43050c27a45a6f36212d2365f900a34487
Mirko Rinke <mirkorinke@htp.com>
2024-11-07 20:28:03 +0100

    Add SalsaBottles class for managing salsa bottle objects in the game

---
+++ js/models/salsa_bottles.class.js
@@ -0,0 +1,11 @@
+import { DrawableObject } from "./drawable-object.class.js";
+
+export class SalsaBottles extends DrawableObject {
+  IMAGES = ["../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png", "../../assets/images/items/salsa_bottle/2_salsa_bottle_on_ground.png"];
+
+  constructor() {
+    super().loadImage("../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png"); // Load the image of the throwable object (salsa bottle)
+    // this.loadImages(this.IMAGES); // Load the images of the throwable object (salsa bottle)
+    this.x = 200 + Math.random() * 1800; // set the x position of the chicken to a random value
+  }
+}

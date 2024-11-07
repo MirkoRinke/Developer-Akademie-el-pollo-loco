5453eb04b6481ac686e5e2f9bd2b25d1b128b8c0
Mirko Rinke <mirkorinke@htp.com>
2024-11-07 16:30:53 +0100

    Add energy attribute and dying animation to Chicken class

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -7,15 +7,18 @@ export class Chicken extends MovableObject {
   height = 80; // height of the chicken
   width = 80; // width of the chicken
   y = 360; // y position of the chicken
+  energy = 2; // energy of the chicken
   // Array of image paths for the chicken walking animation
   IMAGES_WALK = [
     "../../assets/images/enemies/chicken/chicken_normal/1_walk/1_w.png",
     "../../assets/images/enemies/chicken/chicken_normal/1_walk/2_w.png",
     "../../assets/images/enemies/chicken/chicken_normal/1_walk/3_w.png",
   ];
+  IMAGES_DEAD = ["../../assets/images/enemies/chicken/chicken_normal/2_dead/dead.png"];
   constructor() {
     super().loadImage("../../assets/images/enemies/chicken/chicken_normal/1_walk/1_w.png"); // load the image of the chicken using the loadImage method from the MovableObject class
     this.loadImages(this.IMAGES_WALK); // load the images for the walking animation
+    this.loadImages(this.IMAGES_DEAD); // load the images for the dying animation
     this.x = 500 + Math.random() * 1000; // set the x position of the chicken to a random value
     this.speed = 0.15 + Math.random() * 0.33; // set the speed of the chicken to a random value
     this.animate(); // animate the chicken
@@ -37,7 +40,9 @@ export class Chicken extends MovableObject {
   // Animate the chicken by playing the chicken walking animation
   chickenAnimation() {
     setInterval(() => {
-      this.playAnimation(this.IMAGES_WALK);
+      if (this.isDead()) {
+        this.playAnimation(this.IMAGES_DEAD); // play the dying animation if the chicken is dead
+      } else this.playAnimation(this.IMAGES_WALK); // play the walking animation if the chicken is alive
     }, 200);
   }
 }

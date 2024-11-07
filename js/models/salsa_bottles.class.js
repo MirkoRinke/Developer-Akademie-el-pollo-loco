a79389da7e88f245ffce0b32bf6114afce16f8ac
Mirko Rinke <mirkorinke@htp.com>
2024-11-07 21:41:16 +0100

    Adding dimensions to SalsaBottles class and commenting out image resources

--- js/models/salsa_bottles.class.js
+++ js/models/salsa_bottles.class.js
@@ -2,7 +2,10 @@
 import { MovableObject } from "./movable-object-class.js";
 
 export class SalsaBottles extends MovableObject {
-  IMAGES = ["../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png", "../../assets/images/items/salsa_bottle/2_salsa_bottle_on_ground.png"];
+  // IMAGES = ["../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png", "../../assets/images/items/salsa_bottle/2_salsa_bottle_on_ground.png"];
+  height = 60; // height of the chicken
+  width = 60; // width of the chicken
+  y = 380; // y position of the chicken
 
   constructor() {
     super().loadImage("../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png"); // Load the image of the throwable object (salsa bottle)

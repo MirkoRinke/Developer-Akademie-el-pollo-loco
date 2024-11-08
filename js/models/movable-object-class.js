e1b8ba7f2d844fb08ffb184a834c6a3bbfba7c39
Mirko Rinke <mirkorinke@htp.com>
2024-11-08 23:23:14 +0100

    Refactor collision detection in MovableObject class to use dynamic buffers for improved accuracy

--- js/models/movable-object-class.js
+++ js/models/movable-object-class.js
@@ -4,6 +4,10 @@ import { DrawableObject } from "./drawable-object.class.js";
 // import the setStoppableInterval function from the script.js file
 import { setStoppableInterval } from "../script.js";
 
+// Reference to the canvas element
+const canvas = document.getElementById("canvas");
+const canvasHeight = canvas.height;
+
 // Create MovableObject class that extends DrawableObject class
 // This class will be used to create objects that can move
 export class MovableObject extends DrawableObject {
@@ -36,18 +40,23 @@ export class MovableObject extends DrawableObject {
       // If the object is a ThrowableObject
       return true; // Return true
     } else {
-      return this.y < 150; // Return true if the object is above the ground level (150)
+      return this.y < canvasHeight - 349; // Return true if the object is above the ground level (150)
     }
   }
 
   // The isColliding method will be used to check if the object is colliding with another object
   isColliding(movableObject) {
-    let buffer = 0; // Buffer for collision detection
-    if (movableObject.constructor.name === "Chicken") buffer = -25; // Set buffer for Chicken object
-    if (movableObject.constructor.name === "ChickenSmall") buffer = -12; // Set buffer for Chicken object
-    if (movableObject.constructor.name === "SalsaBottles") buffer = -30; // Set buffer for Character object
-    const collisionX = this.x + this.width + buffer > movableObject.x - buffer && this.x - buffer < movableObject.x + movableObject.width + buffer;
-    const collisionY = this.y + this.height + buffer > movableObject.y - buffer && this.y - buffer < movableObject.y + movableObject.height + buffer;
+    const buffers = {
+      Character: { top: -150, bottom: 0, left: -50, right: -50 },
+      Chicken: { top: -25, bottom: -25, left: -25, right: -25 },
+      ChickenSmall: { top: -12, bottom: -12, left: -12, right: -12 },
+      SalsaBottles: { top: -30, bottom: -30, left: -30, right: -30 },
+      Coin: { top: -20, bottom: -20, left: -20, right: -20 },
+    };
+    const bufferThis = buffers[this.constructor.name] || { top: 0, bottom: 0, left: 0, right: 0 };
+    const bufferOther = buffers[movableObject.constructor.name] || { top: 0, bottom: 0, left: 0, right: 0 };
+    const collisionX = this.x + this.width + bufferThis.right > movableObject.x - bufferOther.left && this.x - bufferThis.left < movableObject.x + movableObject.width + bufferOther.right;
+    const collisionY = this.y + this.height + bufferThis.bottom > movableObject.y - bufferOther.top && this.y - bufferThis.top < movableObject.y + movableObject.height + bufferOther.bottom;
     return collisionX && collisionY;
   }
 

e7c16821279a3f012e552ef6baef30881a80c8bb
Mirko Rinke <mirkorinke@htp.com>
2024-11-08 13:30:57 +0100

    Refactor coin and salsa bottle classes to remove unused code and comments

--- js/models/coins.class.js
+++ js/models/coins.class.js
@@ -1,14 +1,11 @@
-// import { DrawableObject } from "./drawable-object.class.js";
 import { MovableObject } from "./movable-object-class.js";
 
 export class Coins extends MovableObject {
-  // IMAGES = ["../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png", "../../assets/images/items/salsa_bottle/2_salsa_bottle_on_ground.png"];
   height = 80; // height of the chicken
   width = 80; // width of the chicken
 
   constructor() {
     super().loadImage("../../assets/images/items/coin/coin_2.png"); // Load the image of the throwable object (salsa bottle)
-    // this.loadImages(this.IMAGES); // Load the images of the throwable object (salsa bottle)
     this.x = 200 + Math.random() * 1800; // set the x position of the chicken to a random value
     this.y = 50 + Math.random() * 200; // set the y position of the chicken to a random value
   }
--- js/models/salsa_bottles.class.js
+++ js/models/salsa_bottles.class.js
@@ -1,15 +1,12 @@
-// import { DrawableObject } from "./drawable-object.class.js";
 import { MovableObject } from "./movable-object-class.js";
 
 export class SalsaBottles extends MovableObject {
-  // IMAGES = ["../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png", "../../assets/images/items/salsa_bottle/2_salsa_bottle_on_ground.png"];
   height = 60; // height of the chicken
   width = 60; // width of the chicken
   y = 380; // y position of the chicken
 
   constructor() {
     super().loadImage("../../assets/images/items/salsa_bottle/1_salsa_bottle_on_ground.png"); // Load the image of the throwable object (salsa bottle)
-    // this.loadImages(this.IMAGES); // Load the images of the throwable object (salsa bottle)
     this.x = 200 + Math.random() * 1800; // set the x position of the chicken to a random value
   }
 }

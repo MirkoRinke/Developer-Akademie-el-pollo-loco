e4712570551c3b3c2ae966c759b16e7d6d7802f0
Mirko Rinke <mirkorinke@htp.com>
2024-11-08 23:37:24 +0100

    Refactor CoinsBar, SalsaBottlesBar, and StatusBarHealth classes to extend StatusBar for improved code reuse and maintainability

--- js/models/salsa-bottles-bar.class.js
+++ js/models/salsa-bottles-bar.class.js
@@ -1,9 +1,10 @@
-// Load DrawableObject class from drawable-object.class.js
-import { DrawableObject } from "./drawable-object.class.js";
+// Load the parent class StatusBar from status-bar.class.js
+import { StatusBar } from "./status-bar.class.js";
 
-// Create SalsaBottlesBar class that extends DrawableObject
-// This class represents the salsa bottles bar in the game
-export class SalsaBottlesBar extends DrawableObject {
+// Create SalsaBottlesBar class that extends StatusBar
+// This class represents the salsa bottles bar in the games
+export class SalsaBottlesBar extends StatusBar {
+  // Create an array of images for the salsa bottles bar
   IMAGES = [
     "../../assets/images/ui/statusbars/1_statusbar/3_statusbar_bottle/orange/0.png",
     "../../assets/images/ui/statusbars/1_statusbar/3_statusbar_bottle/orange/20.png",
@@ -13,39 +14,10 @@ export class SalsaBottlesBar extends DrawableObject {
     "../../assets/images/ui/statusbars/1_statusbar/3_statusbar_bottle/orange/100.png",
   ];
 
-  percentage = 100; // Default percentage
-
   constructor() {
     super(); // Call the super method of the parent class
     this.loadImages(this.IMAGES); // Load images for the salsa bottles bar
-    this.x = 20; // Set x position
     this.y = 50; // Set y position
-    this.width = 200; // Set width
-    this.height = 60; // Set height
     this.setPercentage(0); // Set percentage to 0
-  }
-
-  // Set the percentage of the salsa bottles bar
-  setPercentage(percentage) {
-    this.percentage = percentage; // Set the percentage to the given percentage
-    let path = this.IMAGES[this.resolveImageIndex()]; // Get the path of the image based on the percentage
-    this.img = this.imageCache[path]; // Set the image of the salsa bottles bar
-  }
-
-  // Resolve the image index based on the percentage
-  resolveImageIndex() {
-    if (this.percentage === 100) {
-      return 5;
-    } else if (this.percentage >= 80) {
-      return 4;
-    } else if (this.percentage >= 60) {
-      return 3;
-    } else if (this.percentage >= 40) {
-      return 2;
-    } else if (this.percentage >= 20) {
-      return 1;
-    } else {
-      return 0;
-    }
   }
 }

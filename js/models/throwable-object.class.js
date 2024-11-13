cace109c54a7013ec037b0652a8e828b74fa49ec
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 19:15:16 +0100

    Add JSDoc comments to file

--- js/models/throwable-object.class.js
+++ js/models/throwable-object.class.js
@@ -1,18 +1,33 @@
-// Load MovableObject class from movable-object.class.js
+/**
+ * @module "throwable-object.class.js"
+ */
+
+/**
+ * Import the MovableObject class from the movable-object-class.js module.
+ * Import the setStoppableInterval function from the game.js module.
+ * Import the characterDirection constant from the character.class.js module.
+ */
 import { MovableObject } from "./movable-object-class.js";
-
-// import the setStoppableInterval function from the script.js file
 import { setStoppableInterval } from "../game.js";
-
 import { characterDirection } from "./character.class.js";
 
-// Reference to the canvas element
+/**
+ * Reference to the game's canvas element.
+ * @type {HTMLCanvasElement}
+ */
 const canvas = document.getElementById("canvas");
+
+/**
+ * The height of the canvas element.
+ * @type {number}
+ */
 const canvasHeight = canvas.height;
 
-// ThrowableObject class extends MovableObject class
-// ThrowableObject class is used to create throwable objects
 export class ThrowableObject extends MovableObject {
+  /**
+   * Array of image paths for the rotation animation of a salsa bottle.
+   * @type {string[]}
+   */
   IMAGES_ROTATION = [
     "../../assets/images/items/salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "../../assets/images/items/salsa_bottle/bottle_rotation/2_bottle_rotation.png",
@@ -20,6 +35,10 @@ export class ThrowableObject extends MovableObject {
     "../../assets/images/items/salsa_bottle/bottle_rotation/4_bottle_rotation.png",
   ];
 
+  /**
+   * Array of image paths for the splash animation of a throwable object.
+   * @type {string[]}
+   */
   IMAGES_SPLASH = [
     "../../assets/images/items/salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
     "../../assets/images/items/salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
@@ -29,37 +48,55 @@ export class ThrowableObject extends MovableObject {
     "../../assets/images/items/salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
 
+  /**
+   * Creates an instance of a throwable object.
+   *
+   * @param {number} x - The x-coordinate of the object.
+   * @param {number} y - The y-coordinate of the object.
+   */
   constructor(x, y) {
-    super().loadImage("../../assets/images/items/salsa_bottle/salsa_bottle.png"); // Load the image of the throwable object (salsa bottle)
-    this.loadImages(this.IMAGES_ROTATION); // Load the images for the rotation animation
-    this.loadImages(this.IMAGES_SPLASH); // Load the images for the splash animation
-    this.x = x; // Set the x-coordinate of the throwable object
-    this.y = y; // Set the y-coordinate of the throwable object
-    this.height = 60; // Set the height of the throwable object
-    this.width = 50; // Set the width of the throwable object
-    this.groundY = canvasHeight - 160; // Beispielwert für den Boden
-    this.throw(); // Call the throw method to throw the throwable object
+    super().loadImage("../../assets/images/items/salsa_bottle/salsa_bottle.png");
+    this.loadImages(this.IMAGES_ROTATION);
+    this.loadImages(this.IMAGES_SPLASH);
+    this.x = x;
+    this.y = y;
+    this.height = 60;
+    this.width = 50;
+    this.groundY = canvasHeight - 160;
+    this.throw();
   }
 
-  // Method to apply gravity to the throwable object
+  /**
+   * Throws the object with an initial upward speed and applies gravity.
+   * The object moves horizontally based on the character's direction.
+   * The movement stops when the object hits the ground.
+   */
   throw() {
     this.throwableObjectAnimation();
-    this.speedY = 30; // Set the speed of the throwable object in the y-direction
-    this.applyGravity(); // Call the applyGravity method to apply gravity to the throwable object
+    this.speedY = 30;
+    this.applyGravity();
     const throwDirection = characterDirection;
     setStoppableInterval(() => {
-      if (throwDirection === "right") this.x += 15; // Move the throwable object in the x-direction
-      if (throwDirection === "left") this.x -= 15; // Move the throwable object in the x-direction
-      this.checkIfOnGround(); // Call the checkIfOnGround method to check if the throwable object is on the ground
+      if (throwDirection === "right") this.x += 15;
+      if (throwDirection === "left") this.x -= 15;
+      this.checkIfOnGround();
     }, 25);
   }
 
+  /**
+   * Animates the throwable object by playing the rotation animation at a set interval.
+   * Uses the `setStoppableInterval` function to repeatedly call `playAnimation` with `IMAGES_ROTATION`.
+   */
   throwableObjectAnimation() {
     setStoppableInterval(() => {
-      this.playAnimation(this.IMAGES_ROTATION); // Play the rotation animation
+      this.playAnimation(this.IMAGES_ROTATION);
     }, 100);
   }
 
+  /**
+   * Checks if the object has reached the ground level.
+   * If the object is on the ground, it plays the splash animation.
+   */
   checkIfOnGround() {
     if (this.y >= this.groundY) {
       this.playAnimation(this.IMAGES_SPLASH);

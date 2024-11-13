38ed27cd50387c7d57f547357bbc55ea0cad352e
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 19:37:42 +0100

    Add JSDoc comments to file

--- js/models/status-bar-endboss-health.class.js
+++ js/models/status-bar-endboss-health.class.js
@@ -1,15 +1,20 @@
-// Load the parent class StatusBar from status-bar.class.js
+/**
+ * @module "status-bar-endboss-health.class.js"
+ */
+
+/**
+ * Import the StatusBar class from the status-bar.class.js module.
+ */
 import { StatusBar } from "./status-bar.class.js";
 
-// Reference to the canvas element
 const canvas = document.getElementById("canvas");
-const canvasHeight = canvas.height;
 const canvasWidth = canvas.width;
 
-// Create StatusBarHealth class that extends StatusBar
-// This class represents the health status bar in the game
+/**
+ * Class representing the status bar for the end boss's health.
+ * @extends StatusBar
+ */
 export class StatusBarEndbossHealth extends StatusBar {
-  // Create an array of images for the status bar health
   IMAGES = [
     "../../assets/images/ui/statusbars/2_statusbar_endboss/orange-mirrored/orange0.png",
     "../../assets/images/ui/statusbars/2_statusbar_endboss/orange-mirrored/orange20.png",
@@ -19,11 +24,15 @@ export class StatusBarEndbossHealth extends StatusBar {
     "../../assets/images/ui/statusbars/2_statusbar_endboss/orange-mirrored/orange100.png",
   ];
 
+  /**
+   * Constructs the status bar for the end boss's health.
+   * Initializes the position and sets the health percentage to 100%.
+   */
   constructor() {
-    super(); // Call the constructor of the parent class
-    this.loadImages(this.IMAGES); // Load the images for the status bar from the IMAGES array
-    this.x = canvasWidth - (this.width + 20); // Set the x position of the status bar to 20
-    this.y = 0; // Set the y position of the status bar to 0
-    this.setPercentage(100); // Set the percentage of the status bar to 100
+    super();
+    this.loadImages(this.IMAGES);
+    this.x = canvasWidth - (this.width + 20);
+    this.y = 0;
+    this.setPercentage(100);
   }
 }

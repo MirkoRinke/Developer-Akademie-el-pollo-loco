79a514d062c2622d0d0ed2ffd12d12a886005f0b
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 22:31:55 +0100

    Add JSDoc comments to DangerShield class for improved documentation and clarity

--- js/models/danger_shield.class.js
+++ js/models/danger_shield.class.js
@@ -1,10 +1,24 @@
+/**
+ * @module "danger-shield.class.js"
+ */
+
+/**
+ * Import the MovableObject class from the movable-object-class.js module.
+ */
 import { MovableObject } from "./movable-object-class.js";
 
 const canvas = document.getElementById("canvas");
 const canvasHeight = canvas.height;
 const canvasWidth = canvas.width;
 
+/**
+ * Class representing a DangerShield.
+ * @extends MovableObject
+ */
 export class DangerShield extends MovableObject {
+  /**
+   * Constructs a DangerShield object, loads its image, and sets its dimensions and position.
+   */
   constructor() {
     super().loadImage("../../assets/images/ui/shield/danger.png");
     this.width = 108;

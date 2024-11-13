24c1cabea1594d72a294916935783d3ce98a9278
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 17:33:19 +0100

    Add JSDoc comments to file

--- js/sounds.js
+++ js/sounds.js
@@ -1,27 +1,49 @@
-export let bg_sound = new Audio("../../assets/sounds/music/bg-music.mp3"); // background music
+/**
+ * Background music sound.
+ * @type {Audio}
+ */
+export let bg_sound = new Audio("../../assets/sounds/music/bg-music.mp3");
 
-export let bottle_break_sound = new Audio("../../assets/sounds/sfx/bottle-break.mp3"); // sound for breaking a bottle
-export let bottle_looting_sound = new Audio("../../assets/sounds/sfx/bottle-looting.mp3"); // sound for looting a bottle
+/**
+ * Sound effects for winning and game over events.
+ * @type {Audio}
+ */
+export let win_sound = new Audio("../../assets/sounds/sfx/win.mp3");
+export let game_over_sound = new Audio("../../assets/sounds/sfx/game-over.mp3");
 
-export let chicken_attack_sound = new Audio("../../assets/sounds/sfx/chicken-attack.mp3"); // sound for the chicken attack
-export let chicken_death_sound = new Audio("../../assets/sounds/sfx/chicken-death.mp3"); // sound for the death of a chicken
-export let chicken_sound = new Audio("../../assets/sounds/sfx/chicken.mp3"); // sound for the chicken
-export let fight_sound = new Audio("../../assets/sounds/sfx/fight.mp3"); // sound for fighting
+/**
+ * Sound effects for the player character.
+ * @type {Audio}
+ */
+export let walking_sound = new Audio("../../assets/sounds/sfx/walking.mp3");
+export let jump_sound = new Audio("../../assets/sounds/sfx/jump.mp3");
+export let hurt_sound = new Audio("../../assets/sounds/sfx/hurt.mp3");
+export let snoring_sound = new Audio("../../assets/sounds/sfx/snoring.mp3");
 
-export let coin_sound = new Audio("../../assets/sounds/sfx/coin.mp3"); // sound for the coin
+/**
+ * Sound effects for chicken actions.
+ * @type {Audio}
+ */
+export let chicken_attack_sound = new Audio("../../assets/sounds/sfx/chicken-attack.mp3");
+export let chicken_death_sound = new Audio("../../assets/sounds/sfx/chicken-death.mp3");
+export let chicken_sound = new Audio("../../assets/sounds/sfx/chicken.mp3");
 
-export let hurt_sound = new Audio("../../assets/sounds/sfx/hurt.mp3"); // sound for getting hurt
-export let jump_sound = new Audio("../../assets/sounds/sfx/jump.mp3"); // sound for jumping
-export let snoring_sound = new Audio("../../assets/sounds/sfx/snoring.mp3"); // sound for snoring
-export let walking_sound = new Audio("../../assets/sounds/sfx/walking.mp3"); // sound for walking
+/**
+ * Sound effects for various item interactions.
+ * @type {Audio}
+ */
+export let bottle_break_sound = new Audio("../../assets/sounds/sfx/bottle-break.mp3");
+export let bottle_looting_sound = new Audio("../../assets/sounds/sfx/bottle-looting.mp3");
+export let coin_sound = new Audio("../../assets/sounds/sfx/coin.mp3");
 
-export let win_sound = new Audio("../../assets/sounds/sfx/win.mp3"); // sound for winning
-export let game_over_sound = new Audio("../../assets/sounds/sfx/game-over.mp3"); // sound for game over
-
-export let dominating_sound = new Audio("../../assets/sounds/sfx/killing_sounds/dominating.mp3"); // sound for dominating
-export let double_kill_sound = new Audio("../../assets/sounds/sfx/killing_sounds/double-kill.mp3"); // sound for double kill
-export let first_blood_sound = new Audio("../../assets/sounds/sfx/killing_sounds/first-blood.mp3"); // sound for first blood
-export let killshot_sound = new Audio("../../assets/sounds/sfx/killing_sounds/killshot.mp3"); // sound for killshot
-export let rampage_sound = new Audio("../../assets/sounds/sfx/killing_sounds/rampage.mp3"); // sound for rampage
-export let triple_kill_sound = new Audio("../../assets/sounds/sfx/killing_sounds/triple-kill.mp3"); // sound for triple kill
-export let kill_streak_sound = new Audio("../../assets/sounds/sfx/killing_sounds/kill-streak.mp3"); // sound for kill streak
+/**
+ * Sound effects for various killing streaks and fights.
+ * @type {Audio}
+ */
+export let first_blood_sound = new Audio("../../assets/sounds/sfx/killing_sounds/first-blood.mp3");
+export let double_kill_sound = new Audio("../../assets/sounds/sfx/killing_sounds/double-kill.mp3");
+export let triple_kill_sound = new Audio("../../assets/sounds/sfx/killing_sounds/triple-kill.mp3");
+export let dominating_sound = new Audio("../../assets/sounds/sfx/killing_sounds/dominating.mp3");
+export let kill_streak_sound = new Audio("../../assets/sounds/sfx/killing_sounds/kill-streak.mp3");
+export let rampage_sound = new Audio("../../assets/sounds/sfx/killing_sounds/rampage.mp3");
+export let fight_sound = new Audio("../../assets/sounds/sfx/fight.mp3");

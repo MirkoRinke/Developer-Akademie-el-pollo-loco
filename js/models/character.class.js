52bab5c03bed708d79677f8fb7a3305e6fc72505
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 22:56:49 +0100

    Add JSDoc comments to NewClass for improved documentation and clarity

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,32 +1,36 @@
-// Load the MovableObject class from the movable-object-class.js file
+/**
+ * @module "character.class.js"
+ */
+
+/**
+ * Import the MovableObject class from the movable-object-class.js module.
+ * Import the setStoppableInterval and playSound functions from the game.js module.
+ * Import the resetAlert function from the endboss.class.js module.
+ * Import the hurt_sound, snoring_sound, and walking_sound audio files from the sounds.js module.
+ */
 import { MovableObject } from "./movable-object-class.js";
-
-// import the setStoppableInterval function from the script.js file
 import { setStoppableInterval, playSound } from "../game.js";
-
-// Load the resetAlert function from the endboss.class.js file
 import { resetAlert } from "./endboss.class.js";
-
 import { hurt_sound, snoring_sound, walking_sound } from "../sounds.js";
 
-// Reference to the canvas element
 const canvas = document.getElementById("canvas");
 const canvasHeight = canvas.height;
 const canvasWidth = canvas.width;
 export let characterPostion = 0;
 export let characterDirection = "right";
 
-// Character class is a subclass of MovableObject
-// It is used to create the main character of the game
+/**
+ * Represents a character in the game.
+ * @extends MovableObject
+ */
 export class Character extends MovableObject {
-  height = 300; // height of the character
-  width = 200; // width of the character
-  y = canvasHeight - this.height - 50; // y position of the character
-  speed = 10; // speed of the character
-  energy = 100; //! energy of the character
-  idle_time = 0; // idle time of the character
+  height = 300;
+  width = 200;
+  y = canvasHeight - this.height - 50;
+  speed = 10;
+  energy = 100;
+  idle_time = 0;
 
-  // Arrays of image paths for different animations of the character idling.
   IMAGES_IDLE = [
     "../../assets/images/player/pepe/1_idle/idle/I-1.png",
     "../../assets/images/player/pepe/1_idle/idle/I-2.png",
@@ -53,7 +57,6 @@ export class Character extends MovableObject {
     "../../assets/images/player/pepe/1_idle//long_idle/I-20.png",
   ];
 
-  // Arrays of image paths for different animations of the character walking.
   IMAGES_WALK = [
     "../../assets/images/player/pepe/2_walk/W-21.png",
     "../../assets/images/player/pepe/2_walk/W-22.png",
@@ -62,7 +65,7 @@ export class Character extends MovableObject {
     "../../assets/images/player/pepe/2_walk/W-25.png",
     "../../assets/images/player/pepe/2_walk/W-26.png",
   ];
-  // Arrays of image paths for different animations of the character jumping.
+
   IMAGES_JUMP = [
     "../../assets/images/player/pepe/3_jump/J-31.png",
     "../../assets/images/player/pepe/3_jump/J-32.png",
@@ -74,7 +77,7 @@ export class Character extends MovableObject {
     "../../assets/images/player/pepe/3_jump/J-38.png",
     "../../assets/images/player/pepe/3_jump/J-39.png",
   ];
-  // Arrays of image paths for different animations of the character dying.
+
   IMAGES_DEAD = [
     "../../assets/images/player/pepe/5_dead/D-51.png",
     "../../assets/images/player/pepe/5_dead/D-52.png",
@@ -85,24 +88,32 @@ export class Character extends MovableObject {
   ];
   IMAGES_REMOVE_CHARACTER = ["../../assets/images/player/pepe/5_dead/D-57.png"];
 
-  // Arrays of image paths for different animations of the character being hurt.
   IMAGES_HURT = ["../../assets/images/player/pepe/4_hurt/H-41.png", "../../assets/images/player/pepe/4_hurt/H-42.png", "../../assets/images/player/pepe/4_hurt/H-43.png"];
-  world; // world object
+
+  world;
+
+  /**
+   * Initializes a new character instance by loading images for various states,
+   * checking the character's position, applying gravity, and starting animations.
+   */
   constructor() {
-    super().loadImage("../../assets/images/player/pepe/2_walk/W-21.png"); // load the image of the character using the loadImage method from the MovableObject class
-    this.loadImages(this.IMAGES_IDLE); // load the images for the idle animation
-    this.loadImages(this.IMAGES_IDLE_LONG); // load the images for the long idle animation
-    this.loadImages(this.IMAGES_WALK); // load the images for the walking animation
-    this.loadImages(this.IMAGES_JUMP); // load the images for the jumping animation
-    this.loadImages(this.IMAGES_DEAD); // load the images for the dying animation
-    this.loadImages(this.IMAGES_HURT); // load the images for the being hurt animation
-    this.loadImages(this.IMAGES_REMOVE_CHARACTER); // load the images for the removing animation
-    this.checkCharacterPosition(); // check the character position
-    this.applyGravity(); // apply gravity to the character
-    this.animate(); // animate the character
+    super().loadImage("../../assets/images/player/pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_IDLE_LONG);
+    this.loadImages(this.IMAGES_WALK);
+    this.loadImages(this.IMAGES_JUMP);
+    this.loadImages(this.IMAGES_DEAD);
+    this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_REMOVE_CHARACTER);
+    this.checkCharacterPosition();
+    this.applyGravity();
+    this.animate();
   }
 
-  // Check the character position and reset the alert if the character is out of the screen
+  /**
+   * Periodically checks the character's position and triggers an alert reset if the character moves beyond a certain point.
+   * Uses a stoppable interval to repeatedly check the character's x-coordinate.
+   */
   checkCharacterPosition() {
     setStoppableInterval(() => {
       characterPostion = this.x;
@@ -112,36 +123,57 @@ export class Character extends MovableObject {
     }, 200);
   }
 
-  // Check if the character is above the ground by checking the y position of the character is less than the ground level
+  /**
+   * Checks if the character is above the given enemy.
+   *
+   * @param {Object} enemy - The enemy object to check against.
+   * @param {string} enemy.constructor.name - The name of the enemy's constructor.
+   * @param {number} enemy.y - The y-coordinate of the enemy.
+   * @param {number} enemy.height - The height of the enemy.
+   * @param {number} enemy.x - The x-coordinate of the enemy.
+   * @param {number} enemy.width - The width of the enemy.
+   * @returns {boolean} - Returns true if the character is above the enemy, otherwise false.
+   */
   isAbove(enemy) {
     if (enemy.constructor.name === "Endboss") return;
     if (enemy.constructor.name === "ChickenSmall" && this.playerMoving()) return true;
-    return (
-      this.y + this.height <= enemy.y + enemy.height && // check if the y position of the character is less than the y position of the enemy
-      this.y + this.height > enemy.y && // check if the y position of the character is greater than the y position of the enemy
-      this.x + this.width > enemy.x && // check if the x position of the character is greater than the x position of the enemy
-      this.x < enemy.x + enemy.width && // check if the x position of the character is less than the x position of the enemy
-      this.speedY < 0 // check if the character is moving downwards
-    );
+    return this.y + this.height <= enemy.y + enemy.height && this.y + this.height > enemy.y && this.x + this.width > enemy.x && this.x < enemy.x + enemy.width && this.speedY < 0;
   }
 
-  // Animate the character by managing the character movement and character animation
+  /**
+   * Animates the character by managing its movement and triggering character-specific animations.
+   */
   animate() {
     this.manageCharacterMovement();
     this.characterAnimation();
   }
 
-  // Manage the character movement by checking if the character can move right, left
+  /**
+   * Manages the character's movement by setting intervals to check and execute movement actions.
+   * - Moves the character to the right if possible.
+   * - Moves the character to the left if possible.
+   * - Makes the character jump if possible.
+   * - Adjusts the camera position based on the character's x-coordinate.
+   */
   manageCharacterMovement() {
     setStoppableInterval(() => {
-      if (this.canMoveRight()) this.characterMoveRight(); // check if the character can move right and move the character right
-      if (this.canMoveLeft()) this.characterMoveLeft(); // check if the character can move left and move the character left
-      if (this.canJump()) this.jump(); // check if the character can jump and make the character jump
-      if (this.x < canvasWidth * 1.8) this.world.camera_x = -this.x + 100; // set the camera x position to follow the character
+      if (this.canMoveRight()) this.characterMoveRight();
+      if (this.canMoveLeft()) this.characterMoveLeft();
+      if (this.canJump()) this.jump();
+      if (this.x < canvasWidth * 1.8) this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
   }
 
-  // Animate the character by playing different animations based on the character's state
+  /**
+   * Animates the character by setting up intervals for various animations.
+   * Each animation is checked at a specified interval.
+   *
+   * - Dead animation is checked every 100ms.
+   * - Hurt animation is checked every 200ms.
+   * - Jump animation is checked every 250ms.
+   * - Walk animation is checked every 100ms.
+   * - Idle animation is checked every 250ms.
+   */
   characterAnimation() {
     setStoppableInterval(this.checkDeadAnimation.bind(this), 100);
     setStoppableInterval(this.checkHurtAnimation.bind(this), 200);
@@ -150,19 +182,26 @@ export class Character extends MovableObject {
     setStoppableInterval(this.checkIdleAnimation.bind(this), 250);
   }
 
+  /**
+   * Checks and plays the appropriate dead animation for the character.
+   * If the character is already in the dead animation state, it plays the removal animation.
+   * Otherwise, if the character is dead and not yet in the dead animation state, it plays the dead animation and sets the deadAnimation flag to true.
+   */
   checkDeadAnimation() {
     if (this.deadAnimation) {
       this.playAnimation(this.IMAGES_REMOVE_CHARACTER);
     } else if (this.isDead() && !this.deadAnimation) {
-      // check if the character is dead and play the dying animation
       this.playAnimation(this.IMAGES_DEAD);
       this.deadAnimation = true;
     }
   }
 
+  /**
+   * Checks if the character is hurt and plays the hurt animation and sound if true.
+   * Pauses and resets the snoring sound, then plays the hurt sound.
+   */
   checkHurtAnimation() {
     if (this.isHurt()) {
-      // check if the character is hurt and play the being hurt animation
       this.playAnimation(this.IMAGES_HURT);
       snoring_sound.pause();
       snoring_sound.currentTime = 0;
@@ -170,25 +209,39 @@ export class Character extends MovableObject {
     }
   }
 
+  /**
+   * Checks if the character is above the ground and plays the jump animation if true.
+   * Updates the idle time to the current time.
+   */
   checkJumpAnimation() {
     if (this.isAboveGround()) {
-      // check if the character is above the ground and play the jumping animation
       this.playAnimation(this.IMAGES_JUMP);
       this.idle_time = new Date().getTime();
     }
   }
 
+  /**
+   * Checks if the player is moving and not above ground, then plays the walking animation.
+   * Updates the idle time to the current time.
+   */
   checkWalkAnimation() {
     if (this.playerMoving() && !this.isAboveGround()) {
-      // check if the character is moving right or left and play the walking animation
       this.playAnimation(this.IMAGES_WALK);
       this.idle_time = new Date().getTime();
     }
   }
 
+  /**
+   * Checks the character's idle state and plays the appropriate animation.
+   * If the character is idle for more than 15 seconds, it plays a snoring sound and a long idle animation.
+   * Conditions for idle state:
+   * - Character is not dead
+   * - Character is not hurt
+   * - Character is not above ground
+   * - Character is not moving
+   */
   checkIdleAnimation() {
     if (!this.isDead() && !this.isHurt() && !this.isAboveGround() && !this.playerMoving()) {
-      // play the default image of the character
       this.playAnimation(this.IMAGES_IDLE);
       if (new Date().getTime() - this.idle_time > 15000 && this.idle_time !== 0) {
         this.playSnoringSound();
@@ -197,20 +250,35 @@ export class Character extends MovableObject {
     }
   }
 
+  /**
+   * Plays the snoring sound effect at a low volume.
+   */
   playSnoringSound() {
     playSound(snoring_sound, 0.1);
   }
 
+  /**
+   * Checks if the player is moving.
+   * @returns {boolean} True if the player is moving, otherwise false.
+   */
   playerMoving() {
     return this.world.keyboard.RIGHT || this.world.keyboard.LEFT;
   }
 
-  // Check the character can move right or not by checking the RIGHT key is pressed and the x position of the character is less than the level_end_x
+  /**
+   * Checks if the character can move to the right.
+   *
+   * @returns {boolean} True if the character can move right, otherwise false.
+   */
   canMoveRight() {
     return this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x;
   }
 
-  // Character move right and play the walking sound and set the otherDirection to false
+  /**
+   * Moves the character to the right if not dead.
+   * Pauses and resets the snoring sound, then plays the walking sound.
+   * Sets the character's direction to right.
+   */
   characterMoveRight() {
     if (!this.isDead()) {
       this.moveRight();
@@ -222,12 +290,20 @@ export class Character extends MovableObject {
     }
   }
 
-  // Check the character can move left or not by checking the LEFT key is pressed and the x position of the character is greater than 0
+  /**
+   * Checks if the character can move left.
+   * @returns {boolean} True if the character can move left, otherwise false.
+   */
   canMoveLeft() {
     return this.world.keyboard.LEFT && this.x > 0;
   }
 
-  // Character move left and play the walking sound and set the otherDirection to true
+  /**
+   * Moves the character to the left if not dead.
+   * Pauses the snoring sound and resets its time.
+   * Plays the walking sound.
+   * Sets the character's direction to left.
+   */
   characterMoveLeft() {
     if (!this.isDead()) {
       this.moveLeft();
@@ -239,7 +315,11 @@ export class Character extends MovableObject {
     }
   }
 
-  // Check the character can jump or not by checking the SPACE key is pressed and the character is not above the ground
+  /**
+   * Determines if the character can jump.
+   *
+   * @returns {boolean} True if the character can jump, otherwise false.
+   */
   canJump() {
     return this.world.keyboard.JUMP && !this.isAboveGround();
   }

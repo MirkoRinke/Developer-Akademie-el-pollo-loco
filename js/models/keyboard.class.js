4c4c834c5da39af3bbe49952035d8fd17304fa2e
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 20:57:01 +0100

    Add JSDoc comments to Keyboard class for improved documentation and clarity

--- js/models/keyboard.class.js
+++ js/models/keyboard.class.js
@@ -1,7 +1,14 @@
-// The Keyboard class is used to store the state of the keyboard keys
+/**
+ * @module "keyboard.class.js"
+ */
+
+/**
+ * Represents the state of the keyboard inputs.
+ * @class
+ */
 export class Keyboard {
-  LEFT = false; // Left arrow key for move left
-  RIGHT = false; // Right arrow key for move right
-  JUMP = false; // Space bar for jump
-  THRO = false; // F key for throw the salsa bottle
+  LEFT = false;
+  RIGHT = false;
+  JUMP = false;
+  THRO = false;
 }

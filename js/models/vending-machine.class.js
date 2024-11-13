aca00ec1d74bf2c81669fd5574bc99f08258443e
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 18:37:44 +0100

    Add JSDoc comments to file

--- js/models/vending-machine.class.js
+++ js/models/vending-machine.class.js
@@ -1,20 +1,30 @@
-// Load DrawableObject class from drawable-object.class.js
-// import { DrawableObject } from "./drawable-object.class.js";
+/**
+ * Import the MovableObject class from the movable-object-class.js module.
+ */
 import { MovableObject } from "./movable-object-class.js";
 
-// Reference to the canvas element
+/**
+ * Reference to the game's canvas element.
+ * @type {HTMLCanvasElement}
+ */
 const canvas = document.getElementById("canvas");
+
+/**
+ * The height of the canvas element.
+ * @type {number}
+ */
 const canvasHeight = canvas.height;
 
-// Create StatusBar class that extends DrawableObject
-// This class will be used to create a status bar for the player
 export class VendingMachine extends MovableObject {
+  /**
+   * Creates an instance of the VendingMachine class.
+   * Initializes the vending machine image, dimensions, and position.
+   */
   constructor() {
-    super().loadImage("../../assets/images/ui/vending-machine/2.png"); // load the image of the character using the loadImage method from the MovableObject class
-    this.width = 217; // Set the width of the status bar to 200
-    this.height = 264; // Set the height of the status bar to 60
-    // this.x = canvasWidth * 2.53; // Set the x position of the status bar to 20
-    this.x = 0 - this.width / 2; // Set the x position of the status bar to 0
-    this.y = canvasHeight - this.height - 45; // Set the y position of the status bar to 0
+    super().loadImage("../../assets/images/ui/vending-machine/2.png");
+    this.width = 217;
+    this.height = 264;
+    this.x = 0 - this.width / 2;
+    this.y = canvasHeight - this.height - 45;
   }
 }

dce63655c2581a2c52ec9e7ffd01d7362fd9d5c4
Mirko Rinke <mirkorinke@htp.com>
2024-11-13 20:55:44 +0100

    Add JSDoc comments to Level class for improved documentation and clarity

--- js/models/level.class.js
+++ js/models/level.class.js
@@ -1,25 +1,39 @@
-// Reference to the canvas element
+/**
+ * @module "level.class.js"
+ */
+
 const canvas = document.getElementById("canvas");
-const canvasHeight = canvas.height;
 const canvasWidth = canvas.width;
 
-// The Level class is used to store all the information about a level, such as the enemies, clouds, and background objects.
+/**
+ * Represents a level in the game.
+ */
 export class Level {
-  enemies; // An array of enemy objects
-  salsaBottles; // An array of salsa bottle objects
-  coins; // An array of coin objects
-  clouds; // An array of cloud objects
-  vendingMachine; // An array of vending machine objects
-  backgroundObjects; // An array of background objects
-  level_end_x = canvasWidth * 2.5; // The x-coordinate of the end of the level
+  enemies;
+  salsaBottles;
+  coins;
+  clouds;
+  vendingMachine;
+  backgroundObjects;
+  level_end_x = canvasWidth * 2.5;
 
+  /**
+   * Creates an instance of the Level class.
+   *
+   * @constructor
+   * @param {Array} enemies - The enemies in the level.
+   * @param {Array} salsaBottles - The salsa bottles in the level.
+   * @param {Array} coins - The coins in the level.
+   * @param {Array} clouds - The clouds in the level.
+   * @param {Object} vendingMachine - The vending machine in the level.
+   * @param {Array} backgroundObjects - The background objects in the level.
+   */
   constructor(enemies, salsaBottles, coins, clouds, vendingMachine, backgroundObjects) {
-    // The constructor takes in an array of enemies, clouds, and background objects
-    this.enemies = enemies; // Set the enemies array to the passed in enemies array
-    this.salsaBottles = salsaBottles; // Set the salsa bottles array to the passed in salsa bottles array
-    this.coins = coins; // Set the coins array to the passed in coins array
-    this.clouds = clouds; // Set the clouds array to the passed in clouds array
-    this.vendingMachine = vendingMachine; // Set the vending machine array to the passed in vending machine
-    this.backgroundObjects = backgroundObjects; // Set the background objects array to the passed in background objects array
+    this.enemies = enemies;
+    this.salsaBottles = salsaBottles;
+    this.coins = coins;
+    this.clouds = clouds;
+    this.vendingMachine = vendingMachine;
+    this.backgroundObjects = backgroundObjects;
   }
 }
